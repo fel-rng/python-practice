13bd1470e5a65af4c828bdd241317f202cad1ea2
fel-rng <soranchu.tkt@gmail.com>
2026-07-13 17:37:25 +0900

    Add BMI classification

--- 02_bmi_calculator.py
+++ 02_bmi_calculator.py
@@ -2,9 +2,24 @@ def calc_bmi(height, weight):
     bmi = weight / (height ** 2)
     return bmi
 
+
+def judge_bmi(bmi):
+    if bmi < 18.5:
+        return "低体重"
+    elif 18.5 <= bmi < 25:
+        return "普通体重"
+    elif 25 <= bmi < 30:
+        return "肥満（1度）"
+    elif 30 <= bmi < 35:
+        return "肥満（2度）"
+    elif 35 <= bmi < 40:
+        return "肥満（3度）"
+    else:
+        return "肥満（4度）"
+
     
 def display_result(bmi):
-    print(f"BMIは{bmi:.1f}です。")
+    print(f"BMIは{bmi:.1f}で、判定結果は「{judge_bmi(bmi)}」です。")
 
 
 def main():
